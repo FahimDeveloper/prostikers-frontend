a9667df1816b8c277822441b497f945fb4329576
FahimDeveloper <developerfahim246@gmail.com>
2024-09-16 13:06:18 +0600

    fix an issues

--- src/utils/collectDateStatus.ts
+++ src/utils/collectDateStatus.ts
@@ -1,29 +1,40 @@
 export const collectDateStatus = (
   training_end_date: Date | string,
-  traning_start_date?: Date | string
+  training_start_date?: Date | string
 ) => {
-  if (!traning_start_date) {
-    const currentDate = new Date();
-    const trainingDate = new Date(training_end_date);
-    if (currentDate == trainingDate) {
+  // Helper function to compare dates by year, month, and day
+  const compareDates = (date1: Date, date2: Date) => {
+    return (
+      date1.getFullYear() === date2.getFullYear() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getDate() === date2.getDate()
+    );
+  };
+
+  // Get the current date
+  const currentDate = new Date();
+  const trainingEndDate = new Date(training_end_date);
+
+  if (!training_start_date) {
+    if (compareDates(currentDate, trainingEndDate)) {
       return "today";
-    } else if (currentDate > trainingDate) {
+    } else if (currentDate > trainingEndDate) {
       return "completed";
-    } else if (currentDate < trainingDate) {
+    } else if (currentDate < trainingEndDate) {
       return "upcoming";
     }
   }
-  if (traning_start_date) {
-    const currentDate = new Date();
-    const trainingStartDate = new Date(traning_start_date);
-    const trainingEndDate = new Date(training_end_date);
+
+  if (training_start_date) {
+    const trainingStartDate = new Date(training_start_date);
+
     if (currentDate > trainingEndDate) {
       return "completed";
     } else if (currentDate < trainingStartDate) {
       return "upcoming";
     } else if (
-      currentDate < trainingEndDate &&
-      trainingStartDate > currentDate
+      currentDate >= trainingStartDate &&
+      currentDate <= trainingEndDate
     ) {
       return "running";
     }
