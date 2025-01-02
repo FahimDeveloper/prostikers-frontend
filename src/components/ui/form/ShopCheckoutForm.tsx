766265f601414c0608c284ebf578c2066c0daeee
FahimDeveloper <developerfahim246@gmail.com>
2025-01-02 12:40:55 +0600

    try to build shop checkout details part

--- src/components/ui/form/ShopCheckoutForm.tsx
+++ src/components/ui/form/ShopCheckoutForm.tsx
@@ -1,22 +1,30 @@
-import { Form, Input } from "antd";
-import { useEffect } from "react";
+import { Form, Input, Select } from "antd";
+import { useEffect, useState } from "react";
 import { useAppSelector } from "../../../hooks/useAppHooks";
 import { selectCurrentUser } from "../../../redux/features/auth/authSlice";
+import { City, State } from "country-state-city";
 
 const ShopCheckoutForm = ({ form }: { form: any }) => {
   const user = useAppSelector(selectCurrentUser);
+  const [states, setStates] = useState<any>([]);
+  const [cities, setCities] = useState<any>([]);
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  const [selectedState, setSelectedState] = useState(null);
+  const [selectedCity, setSelectedCity] = useState(null);
+
   useEffect(() => {
-    form.setFieldsValue({
-      user_name: `${user?.first_name} ${user?.last_name}`,
-      email: user?.email,
-      phone: user?.phone,
-      state: user?.state,
-      country: user?.country,
-      city: user?.city,
-      street_address: user?.street_address,
-      zip_code: user?.zip_code,
-    });
-  }, [user, form]);
+    if (selectedCountry) {
+      const stateList = State.getStatesOfCountry(selectedCountry);
+      setStates(stateList);
+    }
+  }, [selectedCountry]);
+
+  useEffect(() => {
+    if (selectedCountry && selectedState) {
+      const cityList = City.getCitiesOfState(selectedCountry, selectedState);
+      setCities(cityList);
+    }
+  }, [selectedState]);
 
   const validateUSPhoneNumber = (_: any, value: string) => {
     const phoneNumberRegex =
@@ -28,7 +36,21 @@ const ShopCheckoutForm = ({ form }: { form: any }) => {
     return Promise.resolve();
   };
   return (
-    <Form form={form} layout="vertical" className="space-y-3">
+    <Form
+      form={form}
+      initialValues={{
+        user_name: `${user?.first_name} ${user?.last_name}`,
+        email: user?.email,
+        phone: user?.phone,
+        // state: user?.state,
+        // country: user?.country,
+        // city: user?.city,
+        street_address: user?.street_address,
+        zip_code: user?.zip_code,
+      }}
+      layout="vertical"
+      className="space-y-3"
+    >
       <Form.Item
         label="Full Name"
         name="user_name"
@@ -66,32 +88,55 @@ const ShopCheckoutForm = ({ form }: { form: any }) => {
         />
       </Form.Item>
       <Form.Item
-        label="Street Address"
-        name="street_address"
-        className="m-0"
-        rules={[{ required: true }]}
-      >
-        <Input className="w-full rounded-full p-2" placeholder="Type here.." />
-      </Form.Item>
-      <Form.Item
         className="m-0"
         label="Country"
         name="country"
         rules={[{ required: true }]}
       >
-        <Input className="w-full rounded-full p-2" placeholder="Type here.." />
-      </Form.Item>
-      <Form.Item
-        label="City"
-        name="city"
-        className="m-0"
-        rules={[{ required: true }]}
-      >
-        <Input className="w-full rounded-full p-2" placeholder="Type here.." />
+        <Select
+          onChange={(value) => setSelectedCountry(value)}
+          placeholder="Select your country"
+          options={[
+            {
+              label: "United States",
+              value: "US",
+            },
+          ]}
+        />
       </Form.Item>
       <Form.Item
         label="State/Province"
         name="state"
+        className="m-0"
+        rules={[{ required: true }]}
+      >
+        <Select
+          onChange={(value) => setSelectedState(value)}
+          placeholder="Select your state"
+          options={states.map((state: any) => ({
+            label: state.name,
+            value: state.isoCode,
+          }))}
+        />
+      </Form.Item>
+      <Form.Item
+        className="m-0"
+        label="City"
+        name="city"
+        rules={[{ required: true }]}
+      >
+        <Select
+          onChange={(value) => setSelectedCity(value)}
+          placeholder="Select your city"
+          options={cities.map((country: any) => ({
+            label: country.name,
+            value: country.name,
+          }))}
+        />
+      </Form.Item>
+      <Form.Item
+        label="Street Address"
+        name="street_address"
         className="m-0"
         rules={[{ required: true }]}
       >
